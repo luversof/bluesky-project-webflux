f8370e736ff378cb6c8cf2e10195590eaa6ab5d1
bluesky <luversof@hanmail.net>
2018-12-05 00:01:51 +0900

    auditing 확인 필요

--- bluesky2-parent/bluesky2-app/bluesky2-app-blog/src/main/java/net/luversof/opensource/data/monbodb/config/BlogReactiveMongoConfiguration.java
+++ bluesky2-parent/bluesky2-app/bluesky2-app-blog/src/main/java/net/luversof/opensource/data/monbodb/config/BlogReactiveMongoConfiguration.java
@@ -1,7 +1,6 @@
 package net.luversof.opensource.data.monbodb.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
@@ -14,7 +13,6 @@ import com.mongodb.reactivestreams.client.MongoClient;
 
 @Configuration
 @EnableReactiveMongoRepositories(basePackages = "net.luversof.blog.repository", reactiveMongoTemplateRef = "blogReactiveMongoTemplate")
-@AutoConfigureAfter
 public class BlogReactiveMongoConfiguration {
 	
 	@Bean
--- bluesky2-parent/bluesky2-opensource/bluesky2-opensource-data-mongodb/src/main/java/net/luversof/opensource/data/mongodb/config/DataMongodbConfig.java
+++ bluesky2-parent/bluesky2-opensource/bluesky2-opensource-data-mongodb/src/main/java/net/luversof/opensource/data/mongodb/config/DataMongodbConfig.java
@@ -2,9 +2,11 @@ package net.luversof.opensource.data.mongodb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 @PropertySource("classpath:mongodb.properties")
+@EnableMongoAuditing
 public class DataMongodbConfig {
 
 }
