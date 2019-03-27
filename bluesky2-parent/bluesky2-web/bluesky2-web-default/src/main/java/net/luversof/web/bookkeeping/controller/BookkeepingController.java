16a0fcfb45e1e13175f2d5f2e2c12ace605c6d52
luver <luver@DESKTOP-VHHSBOB>
2019-03-28 03:09:22 +0900

    .

--- bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/BookkeepingController.java
+++ bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/BookkeepingController.java
@@ -1,13 +1,17 @@
 package net.luversof.web.bookkeeping.controller;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.luversof.bookkeeping.constant.AssetType;
+import net.luversof.bookkeeping.domain.Asset;
 import net.luversof.bookkeeping.domain.Bookkeeping;
 import net.luversof.bookkeeping.service.BookkeepingService;
 import net.luversof.security.core.userdetails.BlueskyUser;
@@ -16,18 +20,17 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/bookkeeping")
+@PreAuthorize("hasRole('ROLE_USER')")
 public class BookkeepingController {
 
 	@Autowired
 	private BookkeepingService bookkeepingService;
 	
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/search/myBookkeeping")
 	public Mono<Bookkeeping> findByUserId(BlueskyUser user) {
 		return bookkeepingService.findByUserId(user.getId()).next();
 	}
 	
-	@PreAuthorize("hasRole('ROLE_USER')")
 	@PostMapping
 	public Mono<Bookkeeping> save(BlueskyUser user) {
 		return bookkeepingService.create(user.getId());
@@ -37,4 +40,19 @@ public class BookkeepingController {
 	public Flux<AssetType> assetTypes() {
 		return Flux.just(AssetType.values());
 	}
+	
+	@PostMapping("/asset")
+	public Mono<Bookkeeping> addAsset(BlueskyUser user, Asset asset) {
+		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
+			return bookkeepingService.addAsset(bookkeeping.getId(), asset);
+		});
+	}
+	
+	@DeleteMapping("/asset/{assetId}")
+	public Mono<Bookkeeping> deleteAsset(BlueskyUser user, @PathVariable ObjectId assetId) {
+		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
+			return bookkeepingService.deleteAsset(bookkeeping.getId(), assetId);
+		});
+	}
+	
 }
