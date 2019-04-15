5efe61e1d7dabe4d1da8ec13a7b275123c60e1f6
bluesky <bluesky@ncsoft.com>
2019-04-15 15:56:30 +0900

    vue mode history build시 동작 가능하도록 설정 변경

--- bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/config/WebFluxConfig.java
+++ bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/config/WebFluxConfig.java
@@ -1,13 +1,23 @@
 package net.luversof.web.config;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ReactiveAdapterRegistry;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,13 +27,13 @@ import net.luversof.web.reactive.result.method.UserDetailsHandlerMethodArgumentR
 @Configuration
 //@EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
-	
+
 //	@Autowired
 //	private ThymeleafReactiveViewResolver thymeleafReactiveViewResolver;
 //	
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@PostConstruct
 	public void postConstruct() {
 		objectMapper.registerModule(new ObjectIdSerializerModule());
@@ -33,12 +43,23 @@ public class WebFluxConfig implements WebFluxConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(false);
 	}
-	
+
 	@Bean
 	public UserDetailsHandlerMethodArgumentResolver userDetailsHandlerMethodArgumentResolver() {
 		return new UserDetailsHandlerMethodArgumentResolver(ReactiveAdapterRegistry.getSharedInstance());
 	}
+
+	@Bean
+	public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
+		return route(GET("/"), forwardIndex(indexHtml))
+				.andRoute(GET("/bookkeeping/**"), forwardIndex(indexHtml));
+	}
+
+	private HandlerFunction<ServerResponse> forwardIndex(final Resource indexHtml) {
+		return request -> ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml);
+	}
 	
+
 //	@Bean
 //	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 //		return new HiddenHttpMethodFilter();
@@ -49,8 +70,6 @@ public class WebFluxConfig implements WebFluxConfigurer {
 //		registry.viewResolver(thymeleafReactiveViewResolver);
 //		WebFluxConfigurer.super.configureViewResolvers(registry);
 //	}
-	
-	
 
 //	@Bean
 //	public RouterFunction<ServerResponse> routes(BlogRepository blogRepository) {
--- bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/index/controller/IndexController.java
+++ bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/index/controller/IndexController.java
@@ -5,16 +5,23 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
-	@GetMapping({ "/", "/index" })
-	public String index() {
-		return "index";
-	}
-	
+//	@GetMapping({ "/", "/index" })
+//	public String index() {
+//		return "index";
+//	}
+
+//	@RequestMapping(value = "/bookkeeping/**", method = RequestMethod.GET)
+//	public String html5Forwarding() {
+//		return "forward:/index.html";
+//	}
+
 	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/testSecurity")
 	public @ResponseBody Authentication testSecurity(@Nullable Authentication authentication) {
