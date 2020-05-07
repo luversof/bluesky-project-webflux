70a0712a40c7cf76a77bf033b609d182fdd040a7
luversof <luversof@hanmail.net>
2020-05-08 06:09:16 +0900

    .

--- bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/BookkeepingController.java
+++ bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/BookkeepingController.java
@@ -1,68 +1,68 @@
-package net.luversof.web.bookkeeping.controller;
-
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import net.luversof.bookkeeping.constant.AssetType;
-import net.luversof.bookkeeping.domain.Asset;
-import net.luversof.bookkeeping.domain.Bookkeeping;
-import net.luversof.bookkeeping.service.BookkeepingService;
-import net.luversof.security.core.userdetails.BlueskyUser;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/api/bookkeeping")
-@PreAuthorize("hasRole('ROLE_USER')")
-public class BookkeepingController {
-
-	@Autowired
-	private BookkeepingService bookkeepingService;
-	
-	@GetMapping("/search/myBookkeeping")
-	public Mono<Bookkeeping> findByUserId(BlueskyUser user) {
-		return bookkeepingService.findByUserId(user.getId()).next();
-	}
-	
-	@PostMapping
-	public Mono<Bookkeeping> save(BlueskyUser user) {
-		return bookkeepingService.create(user.getId());
-	}
-	
-	@GetMapping("/assetTypes")
-	public Flux<AssetType> assetTypes() {
-		return Flux.just(AssetType.values());
-	}
-	
-	@PostMapping("/asset")
-	public Mono<Bookkeeping> addAsset(BlueskyUser user, @RequestBody @Validated(Asset.Create.class) Asset asset) {
-		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
-			return bookkeepingService.addAsset(bookkeeping.getId(), asset);
-		});
-	}
-	
-	@PutMapping("/asset")
-	public Mono<Bookkeeping> updateAsset(BlueskyUser user, @RequestBody @Validated(Asset.Update.class) Asset asset) {
-		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
-			return bookkeepingService.updateAsset(bookkeeping.getId(), asset);
-		});
-	}
-	
-	@DeleteMapping("/asset/{assetId}")
-	public Mono<Bookkeeping> deleteAsset(BlueskyUser user, @PathVariable ObjectId assetId) {
-		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
-			return bookkeepingService.deleteAsset(bookkeeping.getId(), assetId);
-		});
-	}
-	
-}
+//package net.luversof.web.bookkeeping.controller;
+//
+//import org.bson.types.ObjectId;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import net.luversof.bookkeeping.constant.AssetType;
+//import net.luversof.bookkeeping.domain.Asset;
+//import net.luversof.bookkeeping.domain.Bookkeeping;
+//import net.luversof.bookkeeping.service.BookkeepingService;
+//import net.luversof.security.core.userdetails.BlueskyUser;
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/api/bookkeeping")
+//@PreAuthorize("hasRole('ROLE_USER')")
+//public class BookkeepingController {
+//
+//	@Autowired
+//	private BookkeepingService bookkeepingService;
+//	
+//	@GetMapping("/search/myBookkeeping")
+//	public Mono<Bookkeeping> findByUserId(BlueskyUser user) {
+//		return bookkeepingService.findByUserId(user.getId()).next();
+//	}
+//	
+//	@PostMapping
+//	public Mono<Bookkeeping> save(BlueskyUser user) {
+//		return bookkeepingService.create(user.getId());
+//	}
+//	
+//	@GetMapping("/assetTypes")
+//	public Flux<AssetType> assetTypes() {
+//		return Flux.just(AssetType.values());
+//	}
+//	
+//	@PostMapping("/asset")
+//	public Mono<Bookkeeping> addAsset(BlueskyUser user, @RequestBody @Validated(Asset.Create.class) Asset asset) {
+//		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
+//			return bookkeepingService.addAsset(bookkeeping.getId(), asset);
+//		});
+//	}
+//	
+//	@PutMapping("/asset")
+//	public Mono<Bookkeeping> updateAsset(BlueskyUser user, @RequestBody @Validated(Asset.Update.class) Asset asset) {
+//		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
+//			return bookkeepingService.updateAsset(bookkeeping.getId(), asset);
+//		});
+//	}
+//	
+//	@DeleteMapping("/asset/{assetId}")
+//	public Mono<Bookkeeping> deleteAsset(BlueskyUser user, @PathVariable ObjectId assetId) {
+//		return bookkeepingService.findByUserId(user.getId()).next().flatMap(bookkeeping -> {
+//			return bookkeepingService.deleteAsset(bookkeeping.getId(), assetId);
+//		});
+//	}
+//	
+//}
--- bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/EntryController.java
+++ bluesky2-parent/bluesky2-web/bluesky2-web-default/src/main/java/net/luversof/web/bookkeeping/controller/EntryController.java
@@ -1,60 +1,60 @@
-package net.luversof.web.bookkeeping.controller;
-
-import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import net.luversof.bookkeeping.domain.Entry;
-import net.luversof.bookkeeping.service.BookkeepingService;
-import net.luversof.bookkeeping.service.EntryService;
-import net.luversof.security.core.userdetails.BlueskyUser;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-@RestController
-@RequestMapping("/api/bookkeeping/entry")
-@PreAuthorize("hasRole('ROLE_USER')")
-public class EntryController {
-
-	
-	@Autowired
-	private BookkeepingService bookkeepingService;
-	
-	@Autowired
-	private EntryService entryService;
-	
-	@GetMapping("/search/myEntryList")
-	public Flux<Entry> findByAssetId(BlueskyUser user) {
-		return bookkeepingService.findByUserId(user.getId()).next().flatMapMany(bookkeeping -> {
-			return entryService.findByBookkeepingId(bookkeeping.getId());
-		});
-	}
-	
-	@GetMapping("/search/findByAssetId")
-	public Flux<Entry> findByAssetId(@RequestBody Entry entry) {
-		return entryService.findByAssetId(entry);
-	}
-	
-	@PostMapping
-	public Mono<Entry> addEntry(@RequestBody Entry entry) {
-		return entryService.add(entry);
-	}
-	
-	@PutMapping
-	public Mono<Entry> updateEntry(@RequestBody Entry entry) {
-		return entryService.update(entry);
-	}
-	
-	@DeleteMapping("/{entryId}")
-	public Mono<Void> deleteEntry(BlueskyUser user, @PathVariable ObjectId entryId) {
-		return entryService.delete(user.getId(), entryId);
-	}
-}
+//package net.luversof.web.bookkeeping.controller;
+//
+//import org.bson.types.ObjectId;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import net.luversof.bookkeeping.domain.Entry;
+//import net.luversof.bookkeeping.service.BookkeepingService;
+//import net.luversof.bookkeeping.service.EntryService;
+//import net.luversof.security.core.userdetails.BlueskyUser;
+//import reactor.core.publisher.Flux;
+//import reactor.core.publisher.Mono;
+//
+//@RestController
+//@RequestMapping("/api/bookkeeping/entry")
+//@PreAuthorize("hasRole('ROLE_USER')")
+//public class EntryController {
+//
+//	
+//	@Autowired
+//	private BookkeepingService bookkeepingService;
+//	
+//	@Autowired
+//	private EntryService entryService;
+//	
+//	@GetMapping("/search/myEntryList")
+//	public Flux<Entry> findByAssetId(BlueskyUser user) {
+//		return bookkeepingService.findByUserId(user.getId()).next().flatMapMany(bookkeeping -> {
+//			return entryService.findByBookkeepingId(bookkeeping.getId());
+//		});
+//	}
+//	
+//	@GetMapping("/search/findByAssetId")
+//	public Flux<Entry> findByAssetId(@RequestBody Entry entry) {
+//		return entryService.findByAssetId(entry);
+//	}
+//	
+//	@PostMapping
+//	public Mono<Entry> addEntry(@RequestBody Entry entry) {
+//		return entryService.add(entry);
+//	}
+//	
+//	@PutMapping
+//	public Mono<Entry> updateEntry(@RequestBody Entry entry) {
+//		return entryService.update(entry);
+//	}
+//	
+//	@DeleteMapping("/{entryId}")
+//	public Mono<Void> deleteEntry(BlueskyUser user, @PathVariable ObjectId entryId) {
+//		return entryService.delete(user.getId(), entryId);
+//	}
+//}
